a9b6524f42d57df26790708e5416f414091243ea
stefanheintz <stefanheintz@67d1971e-d9d5-11dd-9042-6d746b85d38b>
2009-01-04 10:43:29 +0000

    very first version

---
+++ tags/https:/logging-jmx-service.googlecode.com/svn/tags/1.0.0/logjmxservice/src/test/java/de/stefanheintz/log/jmxservice/TestImpl.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2009 Stefan Heintz
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package de.stefanheintz.log.jmxservice;
+
+import org.apache.log4j.Logger;
+
+public class TestImpl implements Test {
+
+	private static final Logger logger = Logger.getLogger(TestImpl.class);
+	
+	private NoLoggingTest loggingTest;
+
+	public void setLoggingTest(NoLoggingTest loggingTest) {
+		this.loggingTest = loggingTest;
+	}
+
+
+	public void logAllLogĹevels() {
+		while (true) {
+			try {
+				Thread.currentThread().sleep(500L);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			logger.trace("This is the TRACE logging");
+			logger.debug("This is the DEBUG logging");
+			logger.warn("This is the WARN logging");
+			logger.info("This is the INFO logging");
+			logger.error("This is the ERROR logging");		
+			logger.fatal("This is the FATAL logging");
+			
+			loggingTest.methodWithNoLogging();
+		}
+	}
+
+}
