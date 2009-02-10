7dba7741f310ef183406ba96ad378ae22a0279ad
stefanheintz <stefanheintz@67d1971e-d9d5-11dd-9042-6d746b85d38b>
2009-02-10 20:03:19 +0000

    added notification support for changes

--- logjmxservice/src/main/java/de/stefanheintz/log/jmxservice/LoggingConfigImpl.java
+++ logjmxservice/src/main/java/de/stefanheintz/log/jmxservice/LoggingConfigImpl.java
@@ -20,7 +20,17 @@ import java.io.StringWriter;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+import javax.management.ListenerNotFoundException;
+import javax.management.MBeanNotificationInfo;
+import javax.management.Notification;
+import javax.management.NotificationBroadcasterSupport;
+import javax.management.NotificationFilter;
+import javax.management.NotificationListener;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
@@ -28,9 +38,19 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.apache.log4j.config.PropertyPrinter;
 import org.apache.log4j.spi.LoggerRepository;
+import org.springframework.jmx.export.notification.NotificationPublisher;
+import org.springframework.jmx.export.notification.NotificationPublisherAware;
 
-public class LoggingConfigImpl implements LoggingConfig {
+public class LoggingConfigImpl implements LoggingConfig, NotificationPublisherAware {
+	
+	
+	private Map<NotificationType, Long> notificationTypeMap = new HashMap<NotificationType, Long>();
+	private NotificationPublisher publisher;
 
+	public void setNotificationPublisher(NotificationPublisher notificationPublisher) {
+        this.publisher = notificationPublisher;
+    }
+	
 	public String[] getLoggers(String filter) {
 		LoggerRepository r = LogManager.getLoggerRepository();
 
@@ -76,35 +96,51 @@ public class LoggingConfigImpl implements LoggingConfig {
 	public void assignTraceLevel(String target) {
 		assignLogLevel(target, Level.TRACE);
 	}
-	
+
 	private void assignLogLevel(String target, Level level) {
-//		Logger logger = LogManager.exists(target);
-//		if(logger != null) {
-//			logger.setLevel(level);
-//		}
+		String message = level.toString() + " for '" + target + "'";
+		Logger existingLogger = LogManager.exists(target);
+		if(existingLogger != null) {
+			message = "from " + existingLogger.getLevel().toString() + " to " + message; 
+		}
+			 
 		LogManager.getLogger(target).setLevel(level);
-	}
-	
-	public void resetConfiguration() {
-	    LogManager.resetConfiguration();
-	    ClassLoader cl = getClass().getClassLoader();
-	    URL log4jprops = cl.getResource(
-	            "log4j.properties");
-	    if (log4jprops != null) {
-	        (new PropertyConfigurator())
-	                .configure(log4jprops);
-	    }
-	}
-	
-	public String printLog4jConfig() {
-		StringWriter sw = new StringWriter();
-	    PrintWriter pw = new PrintWriter(sw);
-	    PropertyPrinter pp = new PropertyPrinter(pw);
-	    pp.print(pw);
-//	    System.out.println(sw.toString());
-	    return sw.toString();
+		sendNotification(NotificationType.CHANGE_LOG_LEVEL, message);
 	}
 
+	private synchronized void sendNotification(NotificationType notificationType, String message) {
+		Long counter = 0L;
+		if(!notificationTypeMap.containsKey(notificationType))
+			notificationTypeMap.put(notificationType, counter);
+		
+		counter = notificationTypeMap.get(notificationType);
+		notificationTypeMap.put(notificationType, Long.valueOf(counter + 1));
+		
+		Notification notification = new Notification(notificationType.toString(), this, counter);
+		notification.setUserData(message);
+		publisher.sendNotification(notification);
+	}
+
+	public void resetConfiguration() {
+		
+		
+		ClassLoader cl = getClass().getClassLoader();
+		LogManager.resetConfiguration();
+		URL log4jprops = cl.getResource("log4j.properties");
+		if (log4jprops != null) {
+			PropertyConfigurator.configure(log4jprops);
+		}
+		sendNotification(NotificationType.RESET_CONFIGURATION , "used file: " + log4jprops.getFile());
+	}
+
+	public String printLog4jConfig() {
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		PropertyPrinter pp = new PropertyPrinter(pw);
+		pp.print(pw);
+		// System.out.println(sw.toString());
+		return sw.toString();
+	}
 
 
 }
